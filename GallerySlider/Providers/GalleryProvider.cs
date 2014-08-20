74e8ac5dabf46ed8ee3078da742305bbbaf3a1fc
Martin Brenn <brenn@depon.net>
2014-08-19 22:20:44 -0400

    Some pets added

--- GallerySlider/Providers/GalleryProvider.cs
+++ GallerySlider/Providers/GalleryProvider.cs
@@ -38,8 +38,33 @@ namespace GallerySlider.Providers
                     new Image ("Parallelogramm", "images/g/shapes/parallelogramm.png" )
                 });
 
+            var galleryPets = new Gallery(3, "Haustiere", "Bilder aus Wikipedia",
+                new Image[] {
+                    new Image ( "Schwein", "images/g/pets/pig.jpg", 
+                        "http://commons.wikimedia.org/wiki/File:Sow_with_piglet.jpg" ),
+                        
+                    new Image ( "Kuh", "images/g/pets/cow.jpg", 
+                        "http://commons.wikimedia.org/wiki/Domesticated_animals?uselang=de#mediaviewer/File:Koe_in_weiland_bij_Gorssel.JPG" ),
+                        
+                    new Image ( "Esel", "images/g/pets/donkey.jpg", 
+                        "http://commons.wikimedia.org/wiki/Domesticated_animals?uselang=de#mediaviewer/File:Osiol001xx.jpg" ),
+                        
+                    new Image ( "Katze", "images/g/pets/cat.jpg", 
+                        "http://de.wikipedia.org/wiki/Katze#mediaviewer/Datei:Gr%C3%BCne_Augen_einer_Katze.JPG" ),
+                        
+                    new Image ( "Schaf", "images/g/pets/sheep.jpg", 
+                        "http://de.wikipedia.org/wiki/Hausschaf#mediaviewer/Datei:SchafSeite.JPG" ),
+                        
+                    new Image ( "Ziege", "images/g/pets/goat.jpg", 
+                        "http://de.wikipedia.org/wiki/Hausziege#mediaviewer/Datei:Hausziege_04.jpg" ),
+                        
+                    new Image ( "Hund", "images/g/pets/dog.jpg", 
+                        "http://de.wikipedia.org/wiki/Hund#mediaviewer/Datei:Sennenhund.jpg" )                        
+                });
+
             Repository.Add(galleryColor);
             Repository.Add(galleryShapes);
+            Repository.Add(galleryPets);
         }
     }
 }
